46269447c185f40b84f25f6ade837d52505ef915
Noelia Gaona <noeliagaona99@gmail.com>
2022-11-17 21:10:04 +0100

    conexion y api  hecha, routers en proceso

---
+++ api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import penguins_info
+
+
+app = FastAPI()
+app.include_router(penguins_info.router)
+
+@app.get("/")
+def root():
+    return {"mensaje":"Mi primera API"}
+
+
+
---
+++ api/routers/penguins_info.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+from database.conexion_db import db
+from bson import json_util
+from json import loads
+
+router = APIRouter()
+ 
+@router.get("/specie/all")
+def penguin_specie():
+    resp = db["pinguinos_size"].distinct("species")
+    return resp
+
+@router.get("/specie/{specie}")
+def penguin_specie(specie:str):
+    filter = {"species":specie.capitalize()}
+    project = {"_id":0}
+    resp = db["pinguinos_size"].find(filter, project)
+    return loads(json_util.dumps(resp))
+
+@router.get("/island/{island}")
+def penguin_island(island:str):
+    filter = {"island":island.capitalize()}
+    project = {"_id":0, "island":1, "species":1}
+    resp = db["pinguinos_size"].find(filter, project)
+    return loads(json_util.dumps(resp))
+
+@router.get("/sex/{sex}")
+def penguin_island(sex:str):
+    filter = {"sex":sex.upper()}
+    project = {"_id":0, "island":1, "species":1, "sex":1}
+    resp = db["pinguinos_size"].find(filter, project)
+    return loads(json_util.dumps(resp))
+
+@router.get("/culmen")
+def penguin_culmen():
+    filter = {}
+    project = {"_id":0, "species":1, "culmen_length_mm":1, "culmen_depth_mm":1}
+    resp = db["pinguinos_size"].find(filter, project)
+    return loads(json_util.dumps(resp))
